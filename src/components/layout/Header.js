5d46366e6a804eedbb7065a9ddd8d9a33d7b8f2a
Alex Kimutai <billroberts2001@gmail.com>
2020-01-03 19:44:52 +0300

    React app Todo with external backend

---
+++ src/components/layout/Header.js
@@ -0,0 +1,38 @@
+// import React from 'react'
+
+// export default function Header() {
+//     return (
+//         <Header style={headerStyle}>
+//             <h1>TodoList</h1>
+//         </Header>
+//     )
+// }
+// const headerStyle ={
+//     background:'#333',
+//     color:'#fff',
+//     textAlign:'center',
+//     padding:'10px'
+// }
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return(
+        <header style={headerStyle}>
+            <h1>TodoList</h1>
+            <Link style ={linkStyle} to="/">Home</Link> | <Link style ={linkStyle} to="/about">About</Link>
+        </header>
+    )
+}
+const headerStyle ={
+    background:'#333',
+    color:'#fff',
+    textAlign:'center',
+    padding:'10px'
+}
+const linkStyle={
+    textDecoration:'none',
+    color:'#fff'
+}
+export default Header;
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h2>This is about page</h2>
+            <p>this React Application</p>
+        </React.Fragment>
+    )
+}
